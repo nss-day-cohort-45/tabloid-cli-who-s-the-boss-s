b9d16102a364747ead7c50c98a9a3c5e7936d4e8
skgilmore <skg.gilmore@gmail.com>
2021-03-19 15:28:23 -0500

    working through adding new journal entry

---
+++ TabloidCLI/Repositories/JournalRepository.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+using Microsoft.Data.SqlClient;
+
+/// <summary>
+/// Given the user is viewing the Journal Management menu  : TODO add journal mgmt to program.cs
+//When they select the option to add a journal entry: TODO add an option to add journal entry to program.cs
+//Then they should be prompted to enter the new entry's title and content
+//Then the new journal entry should be saved to the database with the current date and time saved as the creation date
+/// </summary>
+namespace TabloidCLI
+{
+    public class JournalRepository : DatabaseConnector, IRepository<Journal>
+    {
+        public JournalRepository(string connectionString) : base(connectionString) { }
+        public List<Journal> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                { 
+                    cmd.CommandText = @"Select id,
+                                            Title,
+                                            Content
+                                            FROM Journal";
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    List<Journal> journals = new List<Journal>();
+
+                    while (reader.Read())
+                    {
+                        Journal journal = new Journal()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            Content = reader.GetString(reader.GetOrdinal("Content")),
+                        };
+                        journals.Add(journal);
+                    }
+
+                    reader.Close();
+
+                    return journals;
+                }
+            }
+        }
+        public Journal Get(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT *
+                                          FROM Journal  
+                                         WHERE id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    Journal journal = null;
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        if (journal == null)
+                        {
+                            journal = new Journal()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Title = reader.GetString(reader.GetOrdinal("Title")),
+                                Content = reader.GetString(reader.GetOrdinal("Content")),
+                            };
+                        }
+
+                    //    if (!reader.IsDBNull(reader.GetOrdinal("TagId")))
+                      //  {
+                        //    journal.Tags.Add(new Tag()
+                          //  {
+                            //    Id = reader.GetInt32(reader.GetOrdinal("TagId")),
+                              //  Name = reader.GetString(reader.GetOrdinal("Name")),
+                           // });
+                        //}
+                    }
+
+                    reader.Close();
+
+                    return journal;
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Journal IRepository<Journal>.Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public void Update(Journal journal)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public void Insert(Journal journal)
+                    {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Journal (Title, Content )
+                                                     VALUES (@title , @content)";
+                    cmd.Parameters.AddWithValue("@title", journal.Title);
+                    cmd.Parameters.AddWithValue("@content", journal.Content);
+
+                    cmd.ExecuteNonQuery();
+
+                }
+            }
+        }
+
+    }
+
+
+    
+                }
+            
---
+++ TabloidCLI/UserInterfaceManagers/JournalManager.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using TabloidCLI.Models;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class JournalManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private JournalRepository _journalRepository;
+        private string _connectionString;
+
+        public JournalManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _journalRepository = new JournalRepository(connectionString);
+            _connectionString = connectionString;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Journal Menu");
+            Console.WriteLine(" 3) Add Journal Entry");
+          //  Console.WriteLine(" 1) List Authors");
+           // Console.WriteLine(" 2) Author Details");
+           // Console.WriteLine(" 4) Edit Author");
+            //Console.WriteLine(" 5) Remove Author");
+            //Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    List();
+                    return this;
+               case "2":
+                    Journal journal = Choose();
+                    if (journal == null)
+                    {
+                        return this;
+                    }
+                    else
+                    {
+                        return new JournalDetailManager(this, _connectionString, journal.Id);
+                    }
+                case "3":
+                    Add();
+                    return this;
+                //case "4":
+                  //  Edit();
+                    //return this;
+                //case "5":
+                  //  Remove();
+                    //return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        public void List()
+        {
+            List<Journal> journals = _journalRepository.GetAll();
+            foreach (Journal journal in journals)
+            {
+                Console.WriteLine(journal);
+            }
+        }
+
+        private Journal Choose(string prompt = null)
+        {
+            if (prompt == null)
+            {
+                prompt = "Please choose a Journal Entry:";
+            }
+
+            Console.WriteLine(prompt);
+
+            List<Journal> journals = _journalRepository.GetAll();
+
+            for (int i = 0; i < journals.Count; i++)
+            {
+                Journal journal = journals[i];
+                Console.WriteLine($" {i + 1}) {journal.Title}");
+            }
+            Console.Write("> ");
+
+            string input = Console.ReadLine();
+            try
+            {
+                int choice = int.Parse(input);
+                return journals[choice - 1];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid Selection");
+                return null;
+            }
+        }
+
+        private void Add()
+        {
+            Console.WriteLine("New Journal Entry");
+            Journal journal = new Journal();
+
+            Console.Write("Title: ");
+            journal.Title = Console.ReadLine();
+
+            Console.Write("Content: ");
+            journal.Content = Console.ReadLine();
+
+       
+
+            _journalRepository.Insert(journal);
+        }
+/// <summary>
+/// end at edit
+/// </summary>
+     
+    }
+}
+
